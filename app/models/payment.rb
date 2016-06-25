0fbd73fc6f514ebe5edea0d8c5746a27a856697d
Julian Feliciano <julsfelic@gmail.com>
2016-06-25 13:18:23 -0600

    Add validation tests to the Payment model
    * Tested for amount formatting, numbericality and presence

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,7 @@
 class Payment < ActiveRecord::Base
   belongs_to :loan
+
+  validates :amount, format: { with: /\A\d+\.?\d{0,2}\z/ }
+  validates :amount, numericality: true
+  validates :amount, presence: true
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:loan) }
+  it { should allow_value(10_000.00).for(:amount) }
+  it { should_not allow_value(10_000.456).for(:amount) }
+  it { should validate_numericality_of(:amount) }
+  it { should validate_presence_of(:amount) }
 end
