6041264f6c6fff8c2b6109326c24f04206c9c1df
Julian Feliciano <julsfelic@gmail.com>
2016-06-25 13:43:46 -0600

    Force amount to be saved as an decimal
    Since our amount will be coming in as a string, I overrided the default
    amount setter to make sure that the value is being converted over to a
    decimal. In the case that we send it nil data or a string that is not
    numeric, I created a helper function numeric? that checks that the
    string is indeed numeric and can be converted over to a decimal

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,4 +4,16 @@ class Payment < ActiveRecord::Base
   validates :amount, format: { with: /\A\d+\.?\d{0,2}\z/ }
   validates :amount, numericality: true
   validates :amount, presence: true
+
+  def amount=(val)
+    write_attribute :amount, val.to_f if numeric?(val)
+  end
+
+  private
+
+  def numeric?(val)
+    !!Kernel.Float(val)
+  rescue TypeError, ArgumentError
+    false
+  end
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Payment, type: :model do
   it { should_not allow_value(10_000.456).for(:amount) }
   it { should validate_numericality_of(:amount) }
   it { should validate_presence_of(:amount) }
+
+  it 'should write amount as a decimal' do
+    payment = Payment.create(amount: '10000.00')
+
+    expect(payment.amount).to eq 10_000.00
+  end
 end
