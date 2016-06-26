3e03bfa6655e367269b6cabe0cb0fe1f1ee04cba
Julian Feliciano <julsfelic@gmail.com>
2016-06-26 12:21:28 -0600

    Add Loan methods for outstanding balance and payment under balance
    * Outstanding balance sums all payment amount and subtracts that by the funded amount
    * `payment_under_balance?` checks if the payment is under the outstanding balance
    * `payment_under_balance?` adds to the payment errors

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Loan, type: :model do
   it { should have_many(:payments) }
+
+  it 'calculates the outstanding_balance' do
+    loan = Loan.create(funded_amount: 10_000.00)
+    loan.payments.create(amount: 5000.00)
+    loan.payments.create(amount: 2000.00)
+
+    expect(loan.outstanding_balance).to eq 3000.0
+  end
+
+  it 'can check if a payment exceeds the loan' do
+    loan = Loan.create(funded_amount: 10_000.00)
+    payment = loan.payments.create(amount: 12_000.00)
+
+    expect(loan.payment_under_balance?(payment)).to eq false
+  end
 end
